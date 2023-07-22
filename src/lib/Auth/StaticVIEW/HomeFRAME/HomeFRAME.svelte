746e8ddc6e9152701a6b58386fdbabe4b273cf7e
Mike John <eviotamikejohnb@gmail.com>
2023-07-22 19:29:42 +0800

    g
    g

--- src/lib/Auth/StaticVIEW/HomeFRAME/HomeFRAME.svelte
+++ src/lib/Auth/StaticVIEW/HomeFRAME/HomeFRAME.svelte
@@ -1,39 +1,24 @@
 <script>
+	import { staticState } from "$lib";
 	import { scale } from "svelte/transition";
 
 
 </script>
 
 <main in:scale>
-        <img loading="lazy" src="https://icct-enrollment-system.vercel.app/_app/immutable/assets/homeIcon.4b30842f.jpg" alt="loading" class="" />
+        
 
-        <section class="flex flex-col gap-2 mt-10">
-                <p>
-                    Today, we mourn the passing of His Excellency Fidel Valdez Ramos "March 18, 1928 - July 31, 2022". We were earnestly blessed to have him as part of our history from the beginning, when ICCT Colleges was established in 1992, the year he became the 12th President of the Republic of the Philippines.
+        <section class="mt-10 p-2 border-[0.1rem] border-black flex flex-col gap-5 bg-gradient-to-bl from-white shadow-lg shadow-black">
+                <p class="text-lg font-semibold  text-center">
+                    Welcome to ICCT Colleges Enrollment System Design and Implementation for Incoming Students for the Year 2023.
                 </p>
 
-                <p>
-                    President Ramos deeply believed in the educative value of “A Global Pinoy Distinction”. In his foreword for the upcoming ICCT Colleges@30 coffee table book, he wrote: “The challenge is for ICCT Colleges to continuously understand the community's needs, the aspirations of its leaders, and the commitment in order for these young minds - the Filipino youth, to realize A Global Pinoy Distinction.
+                <p class="text-base  text-center ">
+                    Where simplicity meets efficiency! Let our friendly chat system guide you through the process seamlessly, making enrollment a breeze. Say goodbye to complexities and hello to hassle-free enrollment. Let's get started!"
                 </p>
 
-                <p>
-                    I trust that we will not be bound by hopes and dreams. We are educators and we must foster these hopes diligently to fulfill its reality. With such aspiration, I will always be supportive to ICCT Colleges and its prime objectives for the Filipino learners of today and tomorrow.”
-                </p>
-
-                <p>
-                    Reminiscing on his message during the ICCT Colleges 4th Founding Anniversary on December 16, 1996, when he said: “We cannot hope to become a modern and newly industrialized country and a globally competitive people without education and training in Computer and Information Technology”.
-                </p>
-
-                <p>
-                    His message inspired the academe and administration of ICCT Colleges to develop ICCT Colleges into one of the most competent education institutions with a large student population in the Philippines.
-                </p>
-
-                <p>
-                    On December 14, 2012, President Ramos graced the 20th Founding Anniversary of ICCT Colleges and the Inauguration Ceremony of ICCT Building 3 and 4 at Cainta, Rizal, Philippines. In his speech, he said: “Your career to serve in the education sector is an obligation. It is really more rewarding and incomparable to any monetary value every time you see the students attaining academic successes and what more, for them to become dignified professionals.”, that was an inspiration for the Institution to keep offering quality and affordable education.
-                </p>
-
-                <p>
-                    In August 26, 2015, it was an honor and privilege for the students to gain knowledge from witnessing President Ramos launch the “EDSA@30: How The EDSA People Power Revolution Transformed the Philippines” at ICCT Colleges Theatre, Cainta, Rizal. The hearts and minds of the ICCT Colleges family will remember all the wonderful times, support and leadership from President Ramos. We will forever be inspired by and connected to his teachings and visions. We now send our love and prayers for his soul to rest in peace.
-                </p>
+                <button class="px-2 py-1 bg-gradient-to-tl from-blue-300 to-pink-700 rounded-md shadow-sm shadow-black active:shadow-none hover:bg-gradient-br hover:from-pink-700 hover:to-blue-300  transition-all active:scale-95 text-white font-semibold"
+                on:click={() => $staticState.activeItem = "Login"}
+                >Lets go!</button>
         </section>
 </main>
